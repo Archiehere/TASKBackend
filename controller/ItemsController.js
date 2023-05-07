168c76621a7783b797ccaf775ff2ed8261c712df
Archiehere <cheeku67@gmail.com>
2023-05-07 22:53:03 +0530

    Bugfix

--- controller/ItemsController.js
+++ controller/ItemsController.js
@@ -32,9 +32,9 @@ const getitem = async (req,res,next) => {
         const {itemId} = req.params;
         if(!itemId)
         return next(new ErrorHandler(400,"All Inputs required"));
-        const item = await item.findByPk(itemId);
+        const myitem = await item.findByPk(itemId);
         
-        return res.status(200).json({success:true,item});
+        return res.status(200).json({success:true,myitem});
     } catch (err) {
         console.log(err);
         next(err);
